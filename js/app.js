63132b998db2e287ca1ff848919424e13a3ab3eb
byehai <89882950+byehai@users.noreply.github.com>
2022-05-19 13:38:17 +1000

    setup
    text input
    add button

---
+++ js/app.js
@@ -0,0 +1,25 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            state: true,
+            inputName: "",
+            names: []
+        }
+    },
+    methods: {
+        addNameToList() {
+            const userName = this.inputName;
+            if (this.validate(userName)) {
+                this.names.push(userName);
+                this.inputName = "";
+                console.log(this.names)
+            } else {
+                
+            }
+        },
+        validate(value) {
+            return true
+        }
+    }
+
+}).mount("#app")
